b2905fe56407c031edf4c2f1873b3cd5dc6f0496
Ajmal Hassan <iamajmalhassan@gmail.com>
2020-07-17 12:18:11 +0530

    Update HealthRecord.py
    Removed unwanted comments

--- HealthRecord.py
+++ HealthRecord.py
@@ -181,62 +181,7 @@ def patient(account):
 			print("\nExiting...\n")
 			time.sleep(1)
 			sys.exit()
-# def patient(account):
-#     while(1):
-# 	    print("What do you want to do?\n")
-# 	    print("1. Create or change .")
-#     	print("2. Gift Tokens to another account.")
-#     	print("3. Redeem your Tokens.")
-#     	print("4. Mint Tokens (You need owner privileges).")
-#     	print("5. Exit")
-#     	c=int(input())
-#     	if(c==1):
-# 		    balance = contract.functions.balance(account).call()
-#     		print("\nBalance is : ",balance)
-# 		    print(" ")
-#     	if(c==2):
-# 		    amount = int(input("Enter the number of Tokens you want to Transfer : "))
-# 		    account2 = input("Enter the account you want to transfer the tokens to : ")
-#     		print(" ")
-# 		    try:
-#     			tx_hash = contract.functions.transferTokens(account2,amount).transact({'from':account})
-# 			    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-#     			print("Transaction Successful.")
-# 			    print("Transaction Hash:  ",web3.toHex(tx_hash))
-# 			    print(" ")
-# 		    except:
-#     			print("The Transaction did not go through. Try again...")
-# 			    time.sleep(2)
 
-#     	if(c==3):
-# 		    amount = int(input("Enter the amount of Tokens :"))
-# 		    print(" ")
-#     		try:
-# 			    tx_hash = contract.functions.redeemTokens(amount).transact({'from':account})
-# 			    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-# 			    print("Transaction Successful.")
-# 			    print("You can show your transaction hash to redeem your gift.")
-# 	    		print("Transaction Hash:  ",web3.toHex(tx_hash))
-# 			    print(" ")
-#     		except:
-# 			    print("The Transaction did not go through. Try again...")
-# 			    time.sleep(2)
-# 	    if(c==4):
-#     		amount = int(input("Enter the amount : "))
-# 		    print(" ")
-# 		    try:
-#     			tx_hash = contract.functions.mint(amount).transact({'from':account})
-# 			    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-# 			    print("Transaction Successful.")
-# 			    print("Transaction Hash: ",web3.toHex(tx_hash))
-# 			    print(" ")
-# 		    except:
-#     			print("Only the owner can mint tokens.")
-# 			    time.sleep(2)
-# 	    if(c==5):
-#     		print("\nExiting...")
-# 		    time.sleep(1)
-# 		    sys.exit()
 if(account==owner or contract.functions.doctor(account).call()==True):
     print("Welcome. You have admin privileges.\n")
     admin(account)
